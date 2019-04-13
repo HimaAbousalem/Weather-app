73b87410d24013b1bd0b40fa894484f6e2146cc4
Ibrahim Abousalem <hima.abousalem@gmail.com>
2019-04-14 01:23:26 +0200

    add ES6 property shorthand and Destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback)=>{
     const url = 'https://api.darksky.net/forecast/7d3bb66ba0543d5c78996f825152dfe9/'+ latitude + ',' + longitude
-    request({url: url, json: true},(error, response)=>{
+    request({url, json: true},(error, {body})=>{
         if(error){
              callback('Unable to connect to weather service!',undefined)
-        }else if(response.body.error){
-            callback(response.body.error,undefined)
+        }else if(body.error){
+            callback(body.error,undefined)
         }else{
-            const currently = response.body.currently
+            const currently = body.currently
             callback(undefined,'it\'s currently '+ currently.temperature+' degrees out.'
              +' There\'s a '+ currently.precipProbability+'% chance of rain.')
         }
