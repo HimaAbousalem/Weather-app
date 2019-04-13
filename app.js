c7595f5a6829a726945b73024575d263f8aa2c42
Ibrahim Abousalem <hima.abousalem@gmail.com>
2019-04-13 19:22:29 +0200

    Chaining callbacks, and handle input throw commandline

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-// const url = 'https://api.darksky.net/forecast/7d3bb66ba0543d5c78996f825152dfe9/37.8267,-122.4233'
+if(!address) return console.log('Please provide an address!')
 
-// request({url: url, json: true}, (error, response)=>{
-//     if(error){
-//         console.log('Unable to connect to weather service!')
-//     }else if(response.body.error){
-//         console.log(response.body.error)
-//     }else{
-//     const currently = response.body.currently
-//     console.log('it\'s currently '+ currently.temperature+' degrees out.'
-//     +' There\'s a '+ currently.precipProbability+'% chance of rain.' )
-//     }
-// })
-
-geocode('cairo',(error, data)=>{
-    console.log('error: ',error )
-    console.log('data: ' , data )
+geocode(address,(error, data)=>{
+    if(error)
+        return console.log(error)
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error) return console.log(error)
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
